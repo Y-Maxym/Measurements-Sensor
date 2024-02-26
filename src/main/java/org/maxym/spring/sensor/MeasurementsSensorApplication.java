379a581506af7ab68b0f1b9df91bb3c067f81c30
Maxym <so.unique.december@gmail.com>
2024-02-26 15:04:22 +0200

    init commit

---
+++ src/main/java/org/maxym/spring/sensor/MeasurementsSensorApplication.java
@@ -0,0 +1,13 @@
+package org.maxym.spring.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementsSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasurementsSensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/maxym/spring/sensor/MeasurementsSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxym.spring.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementsSensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
