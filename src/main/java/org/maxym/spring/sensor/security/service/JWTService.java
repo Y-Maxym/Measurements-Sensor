9b82d72150311fabc1d9babe0ff3f41393f9bad5
Maxym <so.unique.december@gmail.com>
2024-03-05 16:24:09 +0200

    Added: duration for jwt

--- src/main/java/org/maxym/spring/sensor/security/service/JWTService.java
+++ src/main/java/org/maxym/spring/sensor/security/service/JWTService.java
@@ -3,10 +3,12 @@ package org.maxym.spring.sensor.security.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.crypto.SecretKey;
+import java.time.Duration;
 import java.util.Date;
 import java.util.function.Function;
 
@@ -15,21 +17,22 @@ import java.util.function.Function;
 @Transactional(readOnly = true)
 public class JWTService {
 
+    @Value("${jwt.duration}")
+    private Duration duration;
     private final SecretKey key = Jwts.SIG.HS256.key().build();
 
     public String generateToken(String username) {
         return Jwts.builder()
-                .subject("Measurement-Sensor")
-                .claim("username", username)
+                .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .issuer("dec")
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .expiration(new Date(System.currentTimeMillis() + duration.toMillis()))
                 .signWith(key, Jwts.SIG.HS256)
                 .compact();
     }
 
     public String extractUsername(String token) {
-        return extractClaim(token, claims -> claims.get("username", String.class));
+        return extractClaim(token, Claims::getSubject);
     }
 
     private Date extractExpiration(String token) {
