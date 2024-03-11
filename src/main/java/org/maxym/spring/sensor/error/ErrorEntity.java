f72cbde954650089c0fccfcdd78eef7ae7f97df8
Maxym <so.unique.december@gmail.com>
2024-03-11 14:19:55 +0200

    Updated: to record

--- src/main/java/org/maxym/spring/sensor/error/ErrorEntity.java
+++ src/main/java/org/maxym/spring/sensor/error/ErrorEntity.java
@@ -1,14 +1,10 @@
 package org.maxym.spring.sensor.error;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@AllArgsConstructor
-public class ErrorEntity {
-    private String message;
-    private List<FieldErrorEntity> errors;
-    private long timestamp;
+public record ErrorEntity(
+        String message,
+        List<FieldErrorEntity> errors,
+        long timestamp
+) {
 }
--- src/main/java/org/maxym/spring/sensor/error/FieldErrorEntity.java
+++ src/main/java/org/maxym/spring/sensor/error/FieldErrorEntity.java
@@ -1,17 +1,12 @@
 package org.maxym.spring.sensor.error;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.validation.FieldError;
 
-@Data
-@AllArgsConstructor
-public class FieldErrorEntity {
-    private String field;
-    private String message;
-
+public record FieldErrorEntity(
+        String field,
+        String message
+) {
     public FieldErrorEntity(FieldError fieldError) {
-        this.field = fieldError.getField();
-        this.message = fieldError.getDefaultMessage();
+        this(fieldError.getField(), fieldError.getDefaultMessage());
     }
 }
