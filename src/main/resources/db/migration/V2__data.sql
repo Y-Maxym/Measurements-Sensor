4901f1c7e8e0c023c0b52ca2293974d70a63df35
Maxym <so.unique.december@gmail.com>
2024-04-02 16:27:32 +0300

    Added: admin to flyway migration

--- src/main/resources/db/migration/V2__data.sql
+++ src/main/resources/db/migration/V2__data.sql
@@ -1,3 +1,6 @@
 INSERT INTO role(role)
 VALUES ('ROLE_USER'),
        ('ROLE_ADMIN');
+
+INSERT INTO "user"(created_at, email, password, updated_at, username)
+VALUES (NOW(), 'admin@gmail.com', '$2a$10$U2PsxQGqVHGV92E1Di/ww.mZpOTmaO9yeZ1/gJKm543UxWCYd9mVi', NOW(), 'admin');
